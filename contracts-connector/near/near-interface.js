e48dd311aed0b503c7973edb4bf78746346ffb96
AREMSON <habeebtoyin2021@gmail.com>
2022-11-23 15:43:12 +0100

    update

--- contracts-connector/near/near-interface.js
+++ contracts-connector/near/near-interface.js
@@ -27,6 +27,11 @@ export async function nft_mint({ token_id, metadata, receiver_id, perpetual_roya
     return await wallet.callMethod({ method: 'nft_mint', args: { token_id, metadata, receiver_id, perpetual_royalties }, contractId: CONTRACT_ADDRESS });
 }
 
+export async function nft_token({ receiver_id, token_id, approval_id, memo }) {
+    return await wallet.callMethod({ method: 'nft_token', args: { receiver_id, token_id, approval_id, memo }, contractId: CONTRACT_ADDRESS });
+
+ }
+
 export async function nft_transfer({ receiver_id, token_id, approval_id, memo }) {
     return await wallet.callMethod({ method: 'nft_transfer', args: { receiver_id, token_id, approval_id, memo }, contractId: CONTRACT_ADDRESS });
 }
@@ -39,10 +44,116 @@ export async function nft_resolve_transfer({ authorized_id, owner_id, receiver_i
     return await wallet.callMethod({ method: 'nft_resolve_transfer', args: { authorized_id, owner_id, receiver_id, token_id, approved_account_ids, memo }, contractId: CONTRACT_ADDRESS });
 }
 
-export async function nft_transfer({ receiver_id, token_id, approval_id, memo }) {
-    return await wallet.callMethod({ method: 'nft_transfer', args: { receiver_id, token_id, approval_id, memo }, contractId: CONTRACT_ADDRESS });
-}
+ export async function nft_approve({ receiver_id, token_id, approval_id, memo }) {
+    return await wallet.callMethod({ method: 'nft_approve', args: { receiver_id, token_id, approval_id, memo }, contractId: CONTRACT_ADDRESS });
 
+ }
 
+ export async function nft_is_approved({ receiver_id, token_id, approval_id, memo }) {
+    return await wallet.callMethod({ method: 'nft_is_approved', args: { receiver_id, token_id, approval_id, memo }, contractId: CONTRACT_ADDRESS });
 
+ }
+ export async function  nft_payout({ receiver_id, token_id, approval_id, memo }) {
+    return await wallet.callMethod({ method: 'nft_payout', args: { receiver_id, token_id, approval_id, memo }, contractId: CONTRACT_ADDRESS });
+
+ }
+
+ export async function nft_transfer_payout({ receiver_id, token_id, approval_id, memo }) {
+    return await wallet.callMethod({ method: 'nft_transfer_payout', args: { receiver_id, token_id, approval_id, memo }, contractId: CONTRACT_ADDRESS });
+
+ }
+
+ export async function  nft_revoke({ receiver_id, token_id, approval_id, memo }) {
+    return await wallet.callMethod({ method: 'nft_revoke', args: { receiver_id, token_id, approval_id, memo }, contractId: CONTRACT_ADDRESS });
+
+ }
+
+ export async function  nft_revoke_all({ receiver_id, token_id, approval_id, memo }) {
+    return await wallet.callMethod({ method: 'nft_revoke_all', args: { receiver_id, token_id, approval_id, memo }, contractId: CONTRACT_ADDRESS });
+
+ }
+ export async function nft_total_supply({ receiver_id, token_id, approval_id, memo }) {
+    return await wallet.callMethod({ method: 'nft_total_supply', args: { receiver_id, token_id, approval_id, memo }, contractId: CONTRACT_ADDRESS });
+
+ }
+
+ export async function nft_tokens({ receiver_id, token_id, approval_id, memo }) {
+    return await wallet.callMethod({ method: 'nft_tokens', args: { receiver_id, token_id, approval_id, memo }, contractId: CONTRACT_ADDRESS });
+
+ }
+
+ export async function nft_tokens_for_owner({ receiver_id, token_id, approval_id, memo }) {
+    return await wallet.callMethod({ method: 'nft_tokens_for_owner', args: { receiver_id, token_id, approval_id, memo }, contractId: CONTRACT_ADDRESS });
+
+ }
+
+ export async function  nft_supply_for_owner({ receiver_id, token_id, approval_id, memo }) {
+    return await wallet.callMethod({ method: 'nft_supply_for_owner', args: { receiver_id, token_id, approval_id, memo }, contractId: CONTRACT_ADDRESS });
+
+ }
+
+ export async function nft_metadata ({ receiver_id, token_id, approval_id, memo }) {
+    return await wallet.callMethod({ method: 'nft_metadata', args: { receiver_id, token_id, approval_id, memo }, contractId: CONTRACT_ADDRESS });
+
+ }
+
+ export async function  storage_deposit({ receiver_id, token_id, approval_id, memo}) {
+    return await wallet.callMethod({ method: 'storage_deposit', args: { receiver_id, token_id, approval_id, memo }, contractId: CONTRACT_ADDRESS });
+ } 
+
+ export async function storage_withdraw({ receiver_id, token_id, approval_id, memo}) {
+    return await wallet.callMethod({ method: 'storage_withdraw', args: { receiver_id, token_id, approval_id, memo}, contractId: CONTRACT_ADDRESS});
+ }
+
+ export async function storage_minimum_balance({ receiver_id, token_id, approval_id, memo}) {
+    return await wallet.callMethod({ method: 'storage_minimum_balance', args: { receiver_id, token_id, approval_id, memo}, contractId: CONTRACT_ADDRESS});
+ }
+
+ export async function  storage_balance_of({ receiver_id, token_id, approval_id, memo}) {
+    return await wallet.callMethod({ method: ' storage_balance_of', args: { receiver_id, token_id, approval_id, memo}, contractId: CONTRACT_ADDRESS});
+ }
+
+ export async function  remove_sale({ receiver_id, token_id, approval_id, memo}) {
+    return await wallet.callMethod({ method: 'remove_sale', args: { receiver_id, token_id, approval_id, memo}, contractId: CONTRACT_ADDRESS});
+ }
+
+ export async function  update_price({ receiver_id, token_id, approval_id, memo}) {
+    return await wallet.callMethod({ method: 'update_price', args: { receiver_id, token_id, approval_id, memo}, contractId: CONTRACT_ADDRESS});
+ }
+
+ export async function offer({ receiver_id, token_id, approval_id, memo}) {
+    return await wallet.callMethod({ method: 'offer', args: { receiver_id, token_id, approval_id, memo}, contractId: CONTRACT_ADDRESS});
+ }
+
+ export async function resolve_purchase({ receiver_id, token_id, approval_id, memo}) {
+    return await wallet.callMethod({ method: 'resolve_purchase', args: { receiver_id, token_id, approval_id, memo}, contractId: CONTRACT_ADDRESS});
+ }
+
+ export async function get_supply_sales({ receiver_id, token_id, approval_id, memo}) {
+    return await wallet.callMethod({ method: 'get_supply_sales', args: { receiver_id, token_id, approval_id, memo}, contractId: CONTRACT_ADDRESS});
+ }
+
+ export async function get_supply_by_owner_id({ receiver_id, token_id, approval_id, memo}) {
+    return await wallet.callMethod({ method: 'get_supply_by_owner_id', args: { receiver_id, token_id, approval_id, memo}, contractId: CONTRACT_ADDRESS});
+ }
+
+ export async function   get_sales_by_owner_id({ receiver_id, token_id, approval_id, memo}) {
+    return await wallet.callMethod({ method: 'get_sales_by_owner_id', args: { receiver_id, token_id, approval_id, memo}, contractId: CONTRACT_ADDRESS});
+ }
+
+ export async function  get_supply_by_nft_contract_id({ receiver_id, token_id, approval_id, memo}) {
+    return await wallet.callMethod({ method: 'get_supply_by_nft_contract_id', args: { receiver_id, token_id, approval_id, memo}, contractId: CONTRACT_ADDRESS});
+ }
+
+ export async function get_sales_by_nft_contract_id({ receiver_id, token_id, approval_id, memo}) {
+    return await wallet.callMethod({ method: 'get_sales_by_nft_contract_id', args: { receiver_id, token_id, approval_id, memo}, contractId: CONTRACT_ADDRESS});
+ }
+
+ export async function get_sale({ receiver_id, token_id, approval_id, memo}) {
+    return await wallet.callMethod({ method: 'get_sale', args: { receiver_id, token_id, approval_id, memo}, contractId: CONTRACT_ADDRESS});
+ }
+
+ export async function  nft_on_approve({ receiver_id, token_id, approval_id, memo}) {
+    return await wallet.callMethod({ method: 'nft_on_approve', args: { receiver_id, token_id, approval_id, memo}, contractId: CONTRACT_ADDRESS});
+ }
 
