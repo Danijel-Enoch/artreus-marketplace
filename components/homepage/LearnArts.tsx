295588b11b1458ba30303c2804abd2f200a4525b
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-11-24 17:53:53 +0100

    fixed aligments

--- components/homepage/LearnArts.tsx
+++ components/homepage/LearnArts.tsx
@@ -17,7 +17,7 @@ export default function LearnArts() {
       {artsVideos.map((videos,index)=>(
         <div className='inline-block h-max bg-brandpurple w-[250px] rounded-b-[5px] ' key={index}>
           <img  src="/youtube_banner.png" className=' w-[250px]'/>
-          <div className='px-20 py-5'>
+          <div className='flex flex-col justify-center items-center px-20 py-5'>
               <h5 className='font-medium text-base text-white'>Nft Tutorial</h5>
               <h5 className='font-medium text-base text-brandyellow'>Nft Tutorial</h5>
           </div>
