c0331bbad698bcdfd5a5edeeffa8a206292725f7
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-11-24 21:22:16 +0100

    fixed height and widths

--- components/createCollection/ImagePreview.tsx
+++ components/createCollection/ImagePreview.tsx
@@ -3,10 +3,10 @@ import Title from './Title'
 
 export default function ImagePreview({ imageUrl }: { imageUrl: string }) {
   return (
-    <div className="w-[100%] md:mt-3">
+    <div className="w-[100%] h-[100%] md:mt-3">
       <Title className='mb-4 text-bodycopy'>Pre-view</Title>
       <div className='flex justify-center items-center'>
-        <div className='w-[100%] h-[450px] bg-[#ccc] rounded-lg flex justify-center items-center' style={{ backgroundImage: `url(${imageUrl})`, backgroundSize: "100% 100%", backgroundRepeat: "no-repeat",backgroundColor:"#2F2F2F1A" }}>
+        <div className='w-[100%] h-[550px] bg-[#ccc] rounded-lg flex justify-center items-center ' style={{ backgroundImage: `url(${imageUrl})`, backgroundSize: "100% 100%", backgroundRepeat: "no-repeat",backgroundColor:"#2F2F2F1A" }}>
         <p className='text-[#918f8f] text-center px-2'>Preview your recently listed Nfts</p>
         </div>
       </div>
