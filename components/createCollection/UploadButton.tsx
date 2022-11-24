7149dd72beb4eab5fc988be5c803998b9f9aa62d
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-11-24 20:36:20 +0100

    set text color

--- components/createCollection/UploadButton.tsx
+++ components/createCollection/UploadButton.tsx
@@ -8,7 +8,7 @@ export default function UploadButton({ handleChange, ...props }: prop) {
   return (
     <div className='md:order-2 md:mt-4'>
 
-      <Title className='mb-4'>upload file</Title>
+      <Title className='mb-4 text-bodycopy'>Upload file</Title>
       <div className=' text-[#5e5c5c]  border-2 border-dashed p-2  md:p-8 flex flex-col items-center justify-center '>
         Mp4, Png, gif, WEBP or MP3. Max 100Mb
         <label {...props as any} tabIndex={0} role="button" htmlFor='nft-upload' className='bg-brandyellow rounded-md text-black font-bold capitalize mt-4 md:mt-6 py-2 px-4'>choose file</label>
