8e8874bf08ea4c08ca8256b0617e0acc1a21a031
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-11-27 18:08:39 +0100

    fixed nft card size

--- components/homepage/NftCollectionCard.tsx
+++ components/homepage/NftCollectionCard.tsx
@@ -1,8 +1,8 @@
 const NftCollectionCard = ({ name, imageUri, tokenId }: { name: string, imageUri: string, tokenId: string }) => {
   return (
-    <div className='inline-block w-[220px] cursor-pointer -z-10'>
-      <div className="w-full ">
-        <img src={imageUri} className='w-[220px] h-[200px] object-cover rounded-t-md' />
+    <div className='inline-block w-[250px]cursor-pointer -z-10 rounded-[5px]'>
+      <div className="w-full">
+        <img src={imageUri} className='w-[230px] h-[230px] object-cover rounded-t-[5px]' />
       </div>
       <div className='bg-brandpurple p-2 text-white flex flex-col justify-center items-center space-y-2 rounded-b-md'>
         <p className=' font-semibold text-base'>{name} #{tokenId}</p>
