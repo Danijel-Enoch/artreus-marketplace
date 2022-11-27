613f657ca96f1880bad8958cff2ab1e390862e8f
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-11-27 17:06:28 +0100

    set optional chaining

--- components/homepage/LearnArts.tsx
+++ components/homepage/LearnArts.tsx
@@ -11,19 +11,19 @@ export type artVideos = {
 
 
 export default function LearnArts({ learnArts }: { learnArts: artVideos[] }) {
-  const artsVideos = Array.from(Array(10).keys())
+
   return (
     <section>
       <CollectionHeader>
         <SectionTitle title="Learn Arts" />
       </CollectionHeader>
-      <CollectionBody totalItemsLength={artsVideos.length}>
+      <CollectionBody totalItemsLength={learnArts.length}>
         {learnArts.map((videos, index) => (
           <div className='inline-block h-max bg-brandpurple w-[230px] rounded-[5px] ' key={index}>
-            <img src={videos.imageUrl} className='rounded-t-[5px] w-[230px] h-[230px] object-cover' />
+            <img src={videos?.imageUrl} className='rounded-t-[5px] w-[230px] h-[230px] object-cover' />
             <div className='flex flex-col justify-center items-center px-20 py-5'>
-              <h5 className='font-medium text-base text-white'>{videos.name}</h5>
-              <h5 className='font-medium text-base text-brandyellow'>{videos.description}</h5>
+              <h5 className='font-medium text-base text-white'>{videos?.name}</h5>
+              <h5 className='font-medium text-base text-brandyellow'>{videos?.description}</h5>
             </div>
           </div>
         ))}
