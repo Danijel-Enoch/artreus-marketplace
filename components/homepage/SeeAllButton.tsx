55d2465bf386c9c1bedcc6565be3c6dcbd5707ca
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-11-27 15:38:35 +0100

    made see all button visible

--- components/homepage/SeeAllButton.tsx
+++ components/homepage/SeeAllButton.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function SeeAllButton(props:React.HTMLProps<HTMLButtonElement>) {
   return (
-    <div><button  {...props as any} className='invisible md:visible bg-brandpurple py-1 px-4 text-white   rounded-full'>Sell All</button></div>
+    <div><button  {...props as any} className='md:visible bg-brandpurple py-1 px-4 mr-2 text-white   rounded-full'>Sell All</button></div>
   )
 }
