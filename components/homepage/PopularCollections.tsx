ff4d76de0f424d663c386ba15484eacbf3c3d0a6
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-11-27 16:25:18 +0100

    prettified

--- components/homepage/PopularCollections.tsx
+++ components/homepage/PopularCollections.tsx
@@ -13,27 +13,40 @@ const TabHeader = ({ tabTitles }: { tabTitles: string[] }) => {
             {({ selected }) => (
               <button className={`py-2 px-3 border-0 ${selected && 'bg-[#FB005A] m-1 rounded-full pl-4 outline-none'}`}> {title}</button>
             )}
-
           </Tab>
         ))}
-
       </Tab.List>
     </nav>)
 }
 const CollectionPrice = ({ price }: { price: number }) => {
-  return (<div className='flex space-x-2 items-center'>
-    <Image src={`/CMPBlue3D.png`} width={13} height={13} />
-    <p className='text-[#747474]'>{price}</p></div>)
+  return (
+    <div className='flex space-x-2 items-center'>
+      <Image src={`/CMPBlue3D.png`} width={13} height={13} />
+      <p className='text-[#747474]'>{price}</p>
+    </div>
+  )
 
 }
 const Collection = ({ name, index, floorPrice, currentPrice }: { name: string, index: number, floorPrice: number, currentPrice: number }) => {
   const percentage = Math.floor(((currentPrice - floorPrice) / floorPrice) * 100)
   return (
     <div className='flex items-center p-4'>
-      <div className='w-10 h-10  flex items-center space-x-1'><h3 className='font-bold text-bodycopy'>{index}</h3><img src="/profile-picture.png" className='rounded-full w-full h-full' /></div>
-      <div className='ml-8 '><h3 className='font-bold text-brandpurple'>{name}</h3><h4 className='flex items-center space-x-2 text-[#747474]'><p>Floor :</p><CollectionPrice price={floorPrice} /></h4></div>
-      <div className='ml-16 md:ml-10'><p className={`${percentage > 0 ? 'text-green-800' : 'text-red-900'} font-bold  `}>{percentage > 0 && '+'}{percentage} %</p> <CollectionPrice price={currentPrice} /></div>
-    </div>)
+      <div className='w-10 h-10  flex items-center space-x-1'>
+        <h3 className='font-bold text-bodycopy'>{index}</h3>
+        <img src="/profile-picture.png" className='rounded-full w-full h-full' />
+      </div>
+      <div className='ml-8'>
+        <h3 className='font-bold text-brandpurple'>{name}</h3>
+        <h4 className='flex items-center space-x-2 text-[#747474]'>
+          <p>Floor :</p><CollectionPrice price={floorPrice} />
+        </h4>
+      </div>
+      <div className='ml-16 md:ml-10'>
+        <p className={`${percentage > 0 ? 'text-green-800' : 'text-red-900'} font-bold  `}>{percentage > 0 && '+'}{percentage} %</p>
+        <CollectionPrice price={currentPrice} />
+      </div>
+    </div>
+  )
 }
 export default function PopularCollections() {
   const collections = Array.from(Array(12).keys())
@@ -41,7 +54,13 @@ export default function PopularCollections() {
   return (
     <Tab.Group>
       <section className='px-0 md:px-0' >
-        <div className='md:flex  md:justify-between md:items-center'> <div className='space-y-4 md:space-y-0 flex flex-col  md:flex-row md:space-x-10 '><SectionTitle title="Popular Collections" /><TabHeader tabTitles={tabTitles} /> <div></div></div> <SeeAllButton /></div>
+        <div className='md:flex  md:justify-between md:items-center'>
+          <div className='space-y-4 md:space-y-0 flex flex-col  md:flex-row md:space-x-10 '>
+            <SectionTitle title="Popular Collections" />
+            <TabHeader tabTitles={tabTitles} />
+            <div></div>
+          </div> <SeeAllButton />
+        </div>
 
         <Tab.Panels className='px-4'>
           {tabTitles.map((title, index) => (
