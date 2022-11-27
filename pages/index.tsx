9e0d6acbdc4065af592165cd022fcb6d9d5862e6
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-11-27 18:07:38 +0100

    updated dummy data

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,7 @@ import type { NFT } from '../components/homepage/RecentlyListedNfts';
 import HighestSalesNfts from '../components/homepage/HighestSalesNfts'
 import type { highestNFTs } from '../components/homepage/HighestSalesNfts'
 import UpcomingLaunches from '../components/homepage/UpcomingLaunches'
+import type { Upcoming } from '../components/homepage/UpcomingLaunches'
 import useContract from '../hooks/useContract'
 import { useEffect, useState } from 'react'
 import { ArtreusMarketplace } from '../contract-types'
@@ -21,10 +22,11 @@ type props = {
   featuredNfts: FeaturedNft[]
   learnArts: artVideos[]
   recentNFTs: NFT[]
-  highestSoldNFTs : highestNFTs[]
+  highestSoldNFTs: highestNFTs[]
+  upcomingNFTs: Upcoming[]
 }
 
-export default function Home({ newCollectionNfts, featuredNfts, learnArts,recentNFTs,highestSoldNFTs }: props) {
+export default function Home({ newCollectionNfts, featuredNfts, learnArts, recentNFTs, highestSoldNFTs, upcomingNFTs }: props) {
   const [unsoldNFTS, setUnsoldNFTS] = useState<ArtreusMarketplace.ItemStructOutput[]>();
   const instance = useContract();
 
@@ -50,7 +52,7 @@ export default function Home({ newCollectionNfts, featuredNfts, learnArts,recent
         <PopularCollections />
         <RecentlyListedNfts recentNFTs={recentNFTs} />
         <HighestSalesNfts highestSoldNFTs={highestSoldNFTs} />
-        <UpcomingLaunches />
+        <UpcomingLaunches upcomingNFTs={upcomingNFTs} />
       </div>
 
     </>
@@ -146,7 +148,7 @@ export async function getServerSideProps() {
     },
   ]
 
-  const recentNFTs:NFT[] = [
+  const recentNFTs: NFT[] = [
     {
       name: 'Cool Cats',
       price: 10,
@@ -185,41 +187,79 @@ export async function getServerSideProps() {
     },
   ]
 
-  const highestSoldNFTs : highestNFTs[] = [
+  const highestSoldNFTs: highestNFTs[] = [
     {
-      name:'Crypto Birdies',
-      price:3,
-      imageURI:'https://i.seadn.io/gcs/files/154ea199ed44bbe15fad24be55952265.gif?auto=format&w=1920',
+      name: 'Crypto Birdies',
+      price: 3,
+      imageURI: 'https://i.seadn.io/gcs/files/154ea199ed44bbe15fad24be55952265.gif?auto=format&w=1920',
+      tokenId: ''
+    },
+    {
+      name: 'MekaVerse',
+      price: 3,
+      imageURI: 'https://img.seadn.io/files/54a6e47cabe22aca5026f5d529b9b61a.png?auto=format&fit=max&w=512',
+      tokenId: ''
+    },
+    {
+      name: 'Tron Warz',
+      price: 5,
+      imageURI: 'https://i.seadn.io/gcs/files/1e6ac237b1336f329030a3a312049f2a.png?auto=format&w=1920',
+      tokenId: ''
+    },
+    {
+      name: 'TsuKimi',
+      price: 3,
+      imageURI: 'https://i.seadn.io/gcs/files/2dcc373f150631b61e0fd0c45d18bee5.png?auto=format&w=1920',
+      tokenId: ''
+    },
+    {
+      name: 'Fantastic Mysteries',
+      price: 1,
+      imageURI: 'https://i.seadn.io/gae/PY0pGSV5rPsXXePEUeOabUZUwhI3ViUogQdoEiWNYhcY7jxFDssr6IBboUSUKe9ugqlbIa-XDElDFoZXvkkM5PwxA_fijtkpjVL1dg?auto=format&w=512',
+      tokenId: ''
+    },
+    {
+      name: 'Killabits',
+      price: 2,
+      imageURI: 'https://i.seadn.io/gcs/files/da6c85dade2452fbed34b935e1909318.png?auto=format&w=1920',
+      tokenId: ''
+    },
+  ]
+
+  const upcomingNFTs: Upcoming[] = [
+    {
+      name:'DYI-OLD',
+      imageURI:'https://i.seadn.io/gae/7xjqqhHqmsTwpotmW-xj6Suhu2IcpoSXaE6ZSkgSjjeHj6WRBM_U_9sXJIRYA0iGahc8ROXKxpdCemV707Ad5oFuqOjRWHdMviStNMw?auto=format&w=1920',
       tokenId:''
     },
     {
-      name:'MekaVerse',
-      price:3,
-      imageURI:'https://img.seadn.io/files/54a6e47cabe22aca5026f5d529b9b61a.png?auto=format&fit=max&w=512',
-      tokenId:''
-    },
-    {
-      name:'Tron Warz',
-      price:5,
-      imageURI:'https://i.seadn.io/gcs/files/1e6ac237b1336f329030a3a312049f2a.png?auto=format&w=1920',
-      tokenId:''
-    },
-    {
-      name:'TsuKimi',
-      price:3,
-      imageURI:'https://i.seadn.io/gcs/files/2dcc373f150631b61e0fd0c45d18bee5.png?auto=format&w=1920',
-      tokenId:''
-    },
-    {
-      name:'Fantastic Mysteries',
-      price:1,
-      imageURI:'https://i.seadn.io/gae/PY0pGSV5rPsXXePEUeOabUZUwhI3ViUogQdoEiWNYhcY7jxFDssr6IBboUSUKe9ugqlbIa-XDElDFoZXvkkM5PwxA_fijtkpjVL1dg?auto=format&w=512',
-      tokenId:''
-    },
-    {
-      name:'Killabits',
-      price:2,
+      name:'KillaBits',
       imageURI:'https://i.seadn.io/gcs/files/da6c85dade2452fbed34b935e1909318.png?auto=format&w=1920',
+      tokenId:''
+    },
+    {
+      name:'Social-BEEs',
+      imageURI:'https://i.seadn.io/gcs/files/d0cdf15b6de3ff18f047102ef4897fe6.png?auto=format&w=1920',
+      tokenId:''
+    },
+    {
+      name:'GoblinTown',
+      imageURI:'https://i.seadn.io/gae/cb_wdEAmvry_noTfeuQzhqKpghhZWQ_sEhuGS9swM03UM8QMEVJrndu0ZRdLFgGVqEPeCUzOHGTUllxug9U3xdvt0bES6VFdkRCKPqg?auto=format&w=1920',
+      tokenId:''
+    },
+    {
+      name:'Exo Sama',
+      imageURI:'https://i.seadn.io/gcs/files/725700d73c8459bc9e3b29eed95670d8.png?auto=format&w=1920',
+      tokenId:''
+    },
+    {
+      name:'Imposter Aliens',
+      imageURI:'https://i.seadn.io/gae/c-Z_v9W9Ege04QdpjzSsQQIkDR1h4tnBVgwgg9DBuJKdfpmziz9SBOEFBE3EjKW8XESxR_mndEtFFhhxyYUgEwZVAL32uBmQr4H1lQ?auto=format&w=1920',
+      tokenId:''
+    },
+    {
+      name:'StickMen Toys',
+      imageURI:'https://i.seadn.io/gcs/files/2d21367a0bdef1d9db4505e86dba8fdd.gif?auto=format&w=1920',
       tokenId:''
     },
   ]
@@ -230,7 +270,8 @@ export async function getServerSideProps() {
       featuredNfts,
       learnArts,
       recentNFTs,
-      highestSoldNFTs
+      highestSoldNFTs,
+      upcomingNFTs
     }
   }
 }
