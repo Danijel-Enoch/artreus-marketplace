df11a97f51dd5a2343d7ec3cbec38d6350e8df6d
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-12-01 12:56:07 +0100

    changed background color

--- components/createCollection/Input.tsx
+++ components/createCollection/Input.tsx
@@ -4,7 +4,7 @@ export default function Input(props:React.HTMLProps<HTMLInputElement>) {
   return (
     <label className='block md:mt-6 mb-4'>
     {props.label}
-    <input {...props as any} className={`placeholder-black/50 block bg-[#AEACAB] w-full p-2 mt-2 rounded-md outline-none ${props.className&&props.className}`}/>
+    <input {...props as any} className={`placeholder-black/50 block bg-[#2F2F2F1A] w-full p-2 mt-2 rounded-md outline-none ${props.className&&props.className}`}/>
     </label>
   )
 }
