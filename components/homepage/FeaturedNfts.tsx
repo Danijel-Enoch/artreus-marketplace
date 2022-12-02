adbcb7f676bb19b2d2dc26e8ed36ebd8927cb436
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-12-02 16:30:16 +0100

    fixed button color

--- components/homepage/FeaturedNfts.tsx
+++ components/homepage/FeaturedNfts.tsx
@@ -39,7 +39,7 @@ export default function FeaturedNfts({ featuredNfts }: props) {
           <div className='px-6 md:px-0  md:order-1 md:mt-8'>
             <h1 className="text-4xl font-bold md:font-extrabold text-brandpurple mb-5 md:w-[10ch]">{name}</h1>
             <p className='md:w-[25ch] lg:w-[50ch] text-bodycopy'>{description}</p>
-            <button className='font-semibold text-[18px]  bg-brandpurple md:text-xl text-[#fff] md:bg-brandyellow md:text-brandpurple border-0 py-2 px-6 rounded-lg mt-10' onClick={() => { router.push(`/collection/${name.replace(/\s/g, '').toLowerCase()}`) }}>Explore Collection</button>
+            <button className='font-semibold text-[18px]  md:text-xl  bg-brandyellow text-brandpurple border-0 py-2 px-6 rounded-lg mt-10' onClick={() => { router.push(`/collection/${name.replace(/\s/g, '').toLowerCase()}`) }}>Explore Collection</button>
           </div>
         </div>
       ))}
